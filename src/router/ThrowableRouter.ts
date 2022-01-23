fc1a7a9247c35c099d73372ab9f35d8a1678d4cb
Katalina Okano <git@kat.cafe>
2022-01-23 11:11:58 -0500

    use StatusError instead of brute force type

--- src/router/ThrowableRouter.ts
+++ src/router/ThrowableRouter.ts
@@ -1,11 +1,8 @@
 import { Request, RouteHandler, Router, RouterOptions } from 'itty-router'
+import { StatusError } from '../classes'
 import { error } from '../response'
 
 export type ThrowableRouterOptions<T> = RouterOptions<T> & { stack?: boolean }
-
-export type RouterError = Error & {
-  status?: number
-}
 
 export const ThrowableRouter = <T>(options: ThrowableRouterOptions<T> = {}) => {
   const { stack = false } = options
@@ -15,14 +12,17 @@ export const ThrowableRouter = <T>(options: ThrowableRouterOptions<T> = {}) => {
       (obj: Router<T>, prop: string) =>
         (r: Request | string, ...args: unknown[]) => {
           if (prop === 'handle') {
+            return obj.handle(r as Request, ...args).catch((err: StatusError | Error) => {
+              const status = err instanceof StatusError
+                ? err.status || 500
+                : 500
 
-            return obj.handle(r as Request, ...args).catch((err: RouterError) =>
-              error(err.status || 500, {
-                status: err.status || 500,
+              return error(status, {
+                status,
                 error: err.message,
                 stack: (stack && err.stack) || undefined,
-              }))
-
+              })
+            })
           }
 
           return obj[prop as string](r as string, ...args as RouteHandler<Request>[])
